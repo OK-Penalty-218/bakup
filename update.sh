1de978fcdd86cfd72f9841d282f5df58bae6e1ce
OK-Penalty-218 <jrchamp99@gmail.com>
2024-12-31 11:17:57 -1000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define target folders
+BIN_DIR="/usr/local/bin"
+CONFIG_DIR="/etc/bakup"
+
+# Ensure the script is run with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (e.g., sudo ./install.sh)"
+  exit 1
+fi
+
+
+# Updating backup scripts to /usr/local/bin
+echo "Installing backup scripts to $BIN_DIR..."
+cp scripts/* "$BIN_DIR"
+chmod +x "$BIN_DIR"/*
+
+# Updating all other files to /etc/bakup
+echo "Installing configuration files to $CONFIG_DIR..."
+cp config/config.json "$CONFIG_DIR"
+
+# Add crontab jobs
+echo "Setting up automated crontab backup jobs..."
+(crontab -l 2>/dev/null; echo "0 4 * * 7  $BIN_DIR/bakup-weekly") | crontab -
+(crontab -l 2>/dev/null; echo "0 3 * * * $BIN_DIR/bakup-daily") | crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * $BIN_DIR/bakup-hourly") | crontab -
+
+echo "Update complete."
